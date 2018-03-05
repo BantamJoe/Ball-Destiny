e23e7da26fd7c19ba969751b4a569758c55947b1
Joe Chan <103152joee@gmail.com>
2018-03-05 12:00:15 +0800

    DT Upload

---
+++ Assets/Scripts/DT/decisionTree_resultBlock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class decisionTree_resultBlock : MonoBehaviour {
+    public string shape;
+    public Color32 color;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        try{
+            Debug.Log(other.gameObject.tag + "    " + shape + "      " + other.gameObject.GetComponent<SpriteRenderer>().material.color + "      " + color);
+            if (other.gameObject.tag == shape && other.gameObject.GetComponent<SpriteRenderer>().material.color == color) {
+                decisionTree_scoreController.score++;
+                Debug.Log("Correct Shape and Color");
+            }
+            else
+            {
+                decisionTree_scoreController.score--;
+            }
+        }
+        finally{
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/DT/decisionTree_timerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class decisionTree_timerController : MonoBehaviour
+{
+    public static int time;
+
+    Text text;
+
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "NEXT: " + time;
+    }
+}
